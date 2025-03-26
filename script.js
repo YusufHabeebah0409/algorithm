8ed0e60d198e7fc9bf2b7d56f984e5b3932283e5
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-03-26 13:19:22 +0100

    Refactor area calculations for triangle, square, circle, quadrilateral, rhombus, trapezium, and parallelogram in script.js for clarity and consistency

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
 // Area of a Triangle 
-var base = 7;
-var height = 4
-var area =1/2 * base * height 
-console.log(area);
-
+var triangleBase = 7;
+var triangleHeight = 4;
+var triangleArea = 1/2 * triangleBase * triangleHeight;
+console.log("Area of Triangle:", triangleArea);
 
 // Area of a Square 
-var side = 10;
-var area = side * side 
-console.log(area);
+var squareSide = 10;
+var squareArea = squareSide * squareSide;
+console.log("Area of Square:", squareArea);
 
+// Area of a Circle 
+var circleRadius = 7;
+var circleArea = 3.14 * circleRadius * circleRadius;
+console.log("Area of Circle:", circleArea);
 
-// Area of a Cirle 
-var radius = 7
-var area = 3.14 * radius * radius
-console.log(area);
-
-// Area of a Quadilateral 
-var diagonal1 = 6;
-var diagonal2 = 8;
-var area = 1/2 * diagonal1 * diagonal2
-console.log(area);
+// Area of a Quadrilateral 
+var quadDiagonal1 = 6;
+var quadDiagonal2 = 8;
+var quadArea = 1/2 * quadDiagonal1 * quadDiagonal2;
+console.log("Area of Quadrilateral:", quadArea);
 
 // Area of a Rhombus 
-var diagonal1 = 10;
-var diagonal2 = 8;
-var area = 1/2 * diagonal1 * diagonal2
-console.log(area);
+var rhombusDiagonal1 = 10;
+var rhombusDiagonal2 = 8;
+var rhombusArea = 1/2 * rhombusDiagonal1 * rhombusDiagonal2;
+console.log("Area of Rhombus:", rhombusArea);
 
-// Area of Trapezium 
-var base1 = 5;
- var base2 = 7;
- var height = 4;
-var area = 1/2 * (base1 + base2) * height
-console.log(area);
+// Area of a Trapezium 
+var trapeziumBase1 = 5;
+var trapeziumBase2 = 7;
+var trapeziumHeight = 4;
+var trapeziumArea = 1/2 * (trapeziumBase1 + trapeziumBase2) * trapeziumHeight;
+console.log("Area of Trapezium:", trapeziumArea);
 
 // Area of a Parallelogram
-var base = 7;
-var height = 4;
-var area = base * height
-console.log(area);
-
+var parallelogramBase = 7;
+var parallelogramHeight = 4;
+var parallelogramArea = parallelogramBase * parallelogramHeight;
+console.log("Area of Parallelogram:", parallelogramArea);
 
 // Body Mass Index (BMI) 
-
 var weight = 65; 
 var height = 1.75;
-var bmi = weight / (height * height)
-console.log(bmi);
-
+var bmi = weight / (height * height);
+console.log("BMI:", bmi);
