b844fbaef54ec29130d5e60f78343057658f561b
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-03-26 13:06:04 +0100

    Add calculations for areas of various shapes in script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Area of a Triangle 
+var base = 7;
+var height = 4
+var area =1/2 * base * height 
+console.log(area);
+
+
+// Area of a Square 
+var side = 10;
+var area = side * side 
+console.log(area);
+
+
+// Area of a Cirle 
+var radius = 7
+var area = 3.14 * radius * radius
+console.log(area);
+
+// Area of a Quadilateral 
+var diagonal1 = 6;
+var diagonal2 = 8;
+var area = 1/2 * diagonal1 * diagonal2
+console.log(area);
+
+
+
