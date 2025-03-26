472a1821f5fae41d378fbbb3f60c330f1c7ea4fc
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-03-26 13:12:39 +0100

    Add calculations for area of rhombus, trapezium, parallelogram, and BMI in script.js

--- script.js
+++ script.js
@@ -22,5 +22,30 @@ var diagonal2 = 8;
 var area = 1/2 * diagonal1 * diagonal2
 console.log(area);
 
+// Area of a Rhombus 
+var diagonal1 = 10;
+var diagonal2 = 8;
+var area = 1/2 * diagonal1 * diagonal2
+console.log(area);
 
+// Area of Trapezium 
+var base1 = 5;
+ var base2 = 7;
+ var height = 4;
+var area = 1/2 * (base1 + base2) * height
+console.log(area);
+
+// Area of a Parallelogram
+var base = 7;
+var height = 4;
+var area = base * height
+console.log(area);
+
+
+// Body Mass Index (BMI) 
+
+var weight = 65; 
+var height = 1.75;
+var bmi = weight / (height * height)
+console.log(bmi);
 
