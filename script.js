c08f956074c30a12fd9467f6b50f97c0a6308b53
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-03-27 10:33:50 +0100

    Refactor console log statements for area calculations and BMI in script.js for improved readability

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 // Area of a Triangle 
 var triangleBase = 7;
 var triangleHeight = 4;
-var triangleArea = 1/2 * triangleBase * triangleHeight;
-console.log("Area of Triangle:", triangleArea);
+var triangleArea = 1 / 2 * triangleBase * triangleHeight;
+console.log("Area of Triangle:" + triangleArea);
 
 // Area of a Square 
 var squareSide = 10;
 var squareArea = squareSide * squareSide;
-console.log("Area of Square:", squareArea);
+console.log("Area of Square:" + squareArea);
 
 // Area of a Circle 
 var circleRadius = 7;
 var circleArea = 3.14 * circleRadius * circleRadius;
-console.log("Area of Circle:", circleArea);
+console.log("Area of Circle:" + circleArea);
 
 // Area of a Quadrilateral 
 var quadDiagonal1 = 6;
 var quadDiagonal2 = 8;
-var quadArea = 1/2 * quadDiagonal1 * quadDiagonal2;
-console.log("Area of Quadrilateral:", quadArea);
+var quadArea = 1 / 2 * quadDiagonal1 * quadDiagonal2;
+console.log("Area of Quadrilateral:" + quadArea);
 
 // Area of a Rhombus 
 var rhombusDiagonal1 = 10;
 var rhombusDiagonal2 = 8;
-var rhombusArea = 1/2 * rhombusDiagonal1 * rhombusDiagonal2;
-console.log("Area of Rhombus:", rhombusArea);
+var rhombusArea = 1 / 2 * rhombusDiagonal1 * rhombusDiagonal2;
+console.log("Area of Rhombus:" + rhombusArea);
 
 // Area of a Trapezium 
 var trapeziumBase1 = 5;
 var trapeziumBase2 = 7;
 var trapeziumHeight = 4;
-var trapeziumArea = 1/2 * (trapeziumBase1 + trapeziumBase2) * trapeziumHeight;
-console.log("Area of Trapezium:", trapeziumArea);
+var trapeziumArea = 1 / 2 * (trapeziumBase1 + trapeziumBase2) * trapeziumHeight;
+console.log("Area of Trapezium:" + trapeziumArea);
 
 // Area of a Parallelogram
 var parallelogramBase = 7;
 var parallelogramHeight = 4;
 var parallelogramArea = parallelogramBase * parallelogramHeight;
-console.log("Area of Parallelogram:", parallelogramArea);
+console.log("Area of Parallelogram:" + parallelogramArea);
 
 // Body Mass Index (BMI) 
-var weight = 65; 
+var weight = 65;
 var height = 1.75;
 var bmi = weight / (height * height);
-console.log("BMI:", bmi);
+console.log("BMI:" + bmi);
